2d2ab360f4a76c2dd03d1b2fa76bedb0f3729935
MMM <31134295+marti157@users.noreply.github.com>
2017-12-22 20:13:14 +0100

    Initial Upload

---
+++ wifi-kill-mac/WifiKillMac.py
@@ -0,0 +1,10 @@
+"""Read the README.md if you don't know what you are doing.
+By Marti157"""
+
+from os import system
+import time
+
+while True:
+	system("sudo ifconfig en0 down")
+	system("YourSudoPassword")
+	time.sleep(10)
