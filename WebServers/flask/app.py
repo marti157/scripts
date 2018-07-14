9f027ba02df2a69061bb653eacedb0d34469ea4a
MMM <31134295+marti157@users.noreply.github.com>
2018-07-14 17:08:24 +0200

    Add files via upload

---
+++ WebServers/flask/app.py
@@ -0,0 +1,21 @@
+""" By Marti157 (on GitHub) """
+
+# Import the necessary libraries for flask and for creating random numbers
+from flask import Flask, render_template, request
+import random
+
+# Create the flask app
+app = Flask(__name__)
+
+# Define the app route at home ("/") and allow GET and POST request methods
+@app.route('/', methods=["GET", "POST"])
+def index():
+	# Set the text to the default message but change it to a random number if the method is POST
+	text = "Try sending a POST request!"
+	if request.method == "POST":
+		text = str(random.randint(1, 10))
+	return text
+
+# Run the app only after including the libraries
+if __name__ == '__main__':
+    app.run()
